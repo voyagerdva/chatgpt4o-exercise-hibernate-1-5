fae91abe11bade086f864926ce8eca391b7f000e
voyagerdva <mariner-9@yandex.ru>
2024-06-12 11:12:20 +0300

    ChG4o exercise 1 (logging)

---
+++ src/main/java/nn/ru/AppRunner.java
@@ -0,0 +1,59 @@
+package nn.ru;
+
+import nn.ru.entity.Product;
+import nn.ru.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AppRunner {
+    private static final Logger logger = LogManager.getLogger(AppRunner.class);
+    private static final Logger fileLogger = LogManager.getLogger("FileLogger");
+    public static void main(String[] args) {
+        logger.info("Application started");
+        fileLogger.debug("==>  This message will be logged to the file");
+        fileLogger.info("==>  This message will be logged to noth the file and the console");
+        // Create new product
+        Product product = new Product();
+        product.setName("Sample Product 3");
+        product.setPrice(99.99);
+
+        // Save product into db
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(product);
+            transaction.commit();
+        }
+
+//        System.out.println("Current ID of product: " + product.getId());
+//        product.setId(4L);
+
+        // Get product
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Product savedProduct = session.get(Product.class, product.getId());
+            System.out.printf("id: %s, Product: %s, price: %s", savedProduct.getId(), savedProduct.getName(), savedProduct.getPrice());
+
+        }
+
+        // Update product:
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+            Product savedProduct = session.get(Product.class, product.getId());
+            savedProduct.setPrice(79.99);
+            session.update(savedProduct);
+            transaction.commit();
+        }
+
+//        // Delete product:
+//        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+//            Transaction transaction = session.beginTransaction();
+//            Product savedProduct = session.get(Product.class, product.getId());
+//            session.delete(savedProduct);
+//            transaction.commit();
+//        }
+
+
+    }
+}
---
+++ src/main/java/nn/ru/entity/Product.java
@@ -0,0 +1,44 @@
+package nn.ru.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
